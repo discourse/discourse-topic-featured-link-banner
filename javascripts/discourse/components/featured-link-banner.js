05c5feb935f10c8a02972986720e9accc6abeb5e
David Taylor <david@taylorhq.com>
2024-03-27 17:02:09 +0000

    DEV: Update linting (#7)

--- javascripts/discourse/components/featured-link-banner.js
+++ javascripts/discourse/components/featured-link-banner.js
@@ -1,13 +1,5 @@
 import Component from "@ember/component";
-import discourseComputed from "discourse-common/utils/decorators";
-import { inject as service } from "@ember/service";
-import DiscourseURL, { getCategoryAndTagUrl } from "discourse/lib/url";
 
 export default Component.extend({
   tagName: "",
-
-  @discourseComputed()
-  earnedBadges() {
-    console.log(true);
-  },
 });
